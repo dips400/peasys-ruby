d8c314c5e556bcb5e6e42827180fa88670b924dc
RMDIPS <rmolliex@dips400.com>
2025-02-28 09:35:46 +0100

    Prepared for use with ruby ​​on rails

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
 require_relative "../lib/active_record/connection_adapters/peasys_adapter"
+require 'dotenv/load' if Rails.env.development? || Rails.env.test?
+
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.descendants << ActiveRecord::ConnectionAdapters::PeasysAdapter
--- lib/active_record/connection_adapters/peasys_adapter.rb
+++ lib/active_record/connection_adapters/peasys_adapter.rb
@@ -18,7 +18,6 @@ module ActiveRecord
           )
       end
 
-      # Ouvre la connexion
       def self.new_client(config)
         PeaClient.new(
             config[:ip_address],
@@ -30,42 +29,6 @@ module ActiveRecord
             config[:online_version],
             config[:retrieve_statistics]
         )
-      end
-
-      # Exécute une requête SQL
-      def execute(sql, name = nil)
-        log(sql, name) do
-          @client.query(sql)
-        end
-      end
-
-      # Vérifie si la connexion est active
-      def active?
-        @client.connected == "Connected"
-      end
-
-      # Ferme la connexion proprement
-      def disconnect!
-        @client.close
-      end
-
-      # Relance la connexion
-      def reconnect!
-        disconnect!
-        @client = self.class.new_client(@config)
-      end
-
-      # Support des transactions
-      def begin_db_transaction
-        execute("BEGIN")
-      end
-
-      def commit_db_transaction
-        execute("COMMIT")
-      end
-
-      def rollback_db_transaction
-        execute("ROLLBACK")
       end
     end
   end
