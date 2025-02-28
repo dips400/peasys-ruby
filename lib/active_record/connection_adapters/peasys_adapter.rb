3dd8e54392d19c943eb8b855d1b34f6f9a1333e9
RMDIPS <rmolliex@dips400.com>
2025-02-28 12:07:18 +0100

    fixed adapter

--- lib/active_record/connection_adapters/peasys_adapter.rb
+++ lib/active_record/connection_adapters/peasys_adapter.rb
@@ -4,36 +4,37 @@ require "peasys" # Charge la gem peasys-ruby
 module ActiveRecord
   module ConnectionAdapters
     class PeasysAdapter < AbstractAdapter
-      def initialize(connection, logger = nil)
+      def initialize(connection, logger = nil, config = {})
         super(connection, logger)
+
+        @config = config.symbolize_keys
         @client = PeaClient.new(
-            config[:ip_address],
-            config[:partition_name],
-            config[:port],
-            config[:username],
-            config[:password],
-            config[:id_client],
-            config[:online_version],
-            config[:retrieve_statistics]
-          )
+          @config[:ip_address],
+          @config[:partition_name],
+          @config[:port],
+          @config[:username],
+          @config[:password],
+          @config[:id_client],
+          @config[:online_version],
+          @config[:retrieve_statistics]
+        )
       end
 
       def self.new_client(config)
+        config = config.symbolize_keys
         PeaClient.new(
-            config[:ip_address],
-            config[:partition_name],
-            config[:port],
-            config[:username],
-            config[:password],
-            config[:id_client],
-            config[:online_version],
-            config[:retrieve_statistics]
+          config[:ip_address],
+          config[:partition_name],
+          config[:port],
+          config[:username],
+          config[:password],
+          config[:id_client],
+          config[:online_version],
+          config[:retrieve_statistics]
         )
       end
     end
   end
 end
-ActiveRecord::ConnectionAdapters::AbstractAdapter.descendants << ActiveRecord::ConnectionAdapters::PeasysAdapter
 
-# Enregistrer l'adaptateur sous le nom "peasys"
 ActiveRecord::ConnectionAdapters::POOLABLE_CONNECTION_TYPES[:peasys] = ActiveRecord::ConnectionAdapters::PeasysAdapter
